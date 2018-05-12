4aff8b66ad7c671ca4857aa91b3b53ffc6a5a3a1
Cebula Kamil <kontakt@KamilCebula.pl>
2018-05-12 13:05:06 +0200

    Remove icons after clicked hamburger

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,13 @@
 const hamburger = document.getElementById("hamburger");
 const nav = document.getElementById("nav");
 const circleSquare = document.getElementById("circle-square");
-const containerBody = document.getElementById("container-body");
+const containerBody = document.getElementById("full-screen-container");
 const textRow = document.getElementById("text-row");
 const circleSquareRow = document.getElementById("circleSquareRow");
 const socialMediaText = document.getElementById("social-media-text");
+const triangleRow = document.getElementById("triangle-row");
+const icon = document.getElementsByClassName("icon");
+const iconP = document.getElementsByClassName("icon-p");
 let open = false;
 let rotated = false;
 
@@ -20,6 +23,12 @@ const displayMenu = function() {
     textRow.style.display = "none";
     circleSquareRow.style.display = "none";
     socialMediaText.style.display = "none";
+    triangleRow.style.display = "none";
+    for (i=0; i<3; i++) {
+        iconP[i].style.display = "none";
+        icon[i].classList.remove("col-md-2");
+        icon[i].style.padding = "5vh";
+    }
     open = true;
     } else {
         hamburger.style.backgroundImage = "url('../img/hamburger_menu.svg')";
@@ -29,6 +38,12 @@ const displayMenu = function() {
         textRow.style.display = "";
         circleSquareRow.style.display = "";
         socialMediaText.style.display = "";
+        triangleRow.style.display = "";
+        for (i=0; i<3; i++) {
+            iconP[i].style.display = "";
+            icon[i].classList.add("col-md-2");
+            icon[i].style.padding = "";
+        }
         open = false;
     }
 }
